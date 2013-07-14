1f60a24d88fba05377836ade7ac144be14aded81
ibictts <ibictts@gmail.com>
2013-07-14 23:08:58 +0000

    read from file then output

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+buff = fs.readFileSync('index.html');
+mystring = buff.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(mystring);
 });
 
 var port = process.env.PORT || 5000;
